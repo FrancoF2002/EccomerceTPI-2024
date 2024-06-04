4e2db39e3a977951c64d59abed5ecbe9408534cc
FrancoF2002 <elpanqui44@gmail.com>
2024-06-04 19:06:07 -0300

    Packages Nugget

--- EccomerceTPI2024/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ EccomerceTPI2024/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd8a70796f6d8cfb02f924865298f1c452e0308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32a2b3ad19cb935e4eb085d89770e4fac47ce82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
