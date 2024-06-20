26f424be0cd59d70ce6e28f6213ff989614c57de
FrancoF2002 <elpanqui44@gmail.com>
2024-06-20 13:57:40 -0300

    msj

---
+++ EccomerceTPI2024/Infrastructure/Migrations/20240620162943_migration new.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationnew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Users_ClientUserId",
+                table: "Order");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Order_OrderId",
+                table: "OrderProduct");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_ClientUserId",
+                table: "Orders",
+                newName: "IX_Orders_ClientUserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Orders_OrderId",
+                table: "OrderProduct",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_ClientUserId",
+                table: "Orders",
+                column: "ClientUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Orders_OrderId",
+                table: "OrderProduct");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_ClientUserId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_ClientUserId",
+                table: "Order",
+                newName: "IX_Order_ClientUserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Users_ClientUserId",
+                table: "Order",
+                column: "ClientUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Order_OrderId",
+                table: "OrderProduct",
+                column: "OrderId",
+                principalTable: "Order",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
